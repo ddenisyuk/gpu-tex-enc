52b9b38c32527a833e56faa2902698337dee6dc9
ddenysiuk <ddenysiuk@playtika.com>
2024-02-15 17:02:58 +0100

    types fixes

--- packages/gpu-tex-enc/src/types.d.ts
+++ packages/gpu-tex-enc/src/types.d.ts
@@ -1,43 +1,43 @@
-import {BlockSize, ColorProfile, Quality} from '../../astc-enc'
-import {ErrorMetric, Format} from '../../etc2-enc'
-import {Type} from '../../bc7-enc'
+import {BlockSize, ColorProfile, Quality} from '@gpu-tex-enc/astc'
+import {ErrorMetric, Format} from '@gpu-tex-enc/etc'
+import {Type} from '@gpu-tex-enc/bc'
 
 export type AstcOptions = {
-  blocksize?: BlockSize,
-  quality?: Quality,
-  colorProfile?: ColorProfile,
-  options?: string[]
+    blocksize?: BlockSize,
+    quality?: Quality,
+    colorProfile?: ColorProfile,
+    options?: string[]
 };
 
 export type EtcOptions = {
-  effort?: number,
-  errormetric?: ErrorMetric,
-  options?: string[]
+    effort?: number,
+    errormetric?: ErrorMetric,
+    options?: string[]
 };
 
 export type BcOptions = {
-  options?: string[]
+    options?: string[]
 };
 
 export type OutputOptions = {
-  'ASTC'?: AstcOptions,
-  'BC1'?: BcOptions,
-  'BC3'?: BcOptions,
-  'BC4'?: BcOptions,
-  'BC5'?: BcOptions,
-  'ETC1'?: EtcOptions,
-  'RGB8'?: EtcOptions,
-  'SRGB8'?: EtcOptions,
-  'RGBA8'?: EtcOptions,
-  'RGB8A1'?: EtcOptions,
-  'SRGB8A1'?: EtcOptions,
-  'R11'?: EtcOptions
+    'ASTC'?: AstcOptions,
+    'BC1'?: BcOptions,
+    'BC3'?: BcOptions,
+    'BC4'?: BcOptions,
+    'BC5'?: BcOptions,
+    'ETC1'?: EtcOptions,
+    'RGB8'?: EtcOptions,
+    'SRGB8'?: EtcOptions,
+    'RGBA8'?: EtcOptions,
+    'RGB8A1'?: EtcOptions,
+    'SRGB8A1'?: EtcOptions,
+    'R11'?: EtcOptions
 };
 
-export function generate(input: string, outputOptions: OutputOptions): string;
+export function generate(input: string, outputOptions: OutputOptions): Promise<string>;
 
-export function generateBc(input: string, type?: Type, adjust?: boolean, options?: string[]): string;
+export function generateBC(input: string, type?: Type, adjust?: boolean, options?: string[]): Promise<string>;
 
-export function generateAstc(input: string, blockSize?: BlockSize, quality?: Quality, colorProfile?: ColorProfile, options?: string[]): string;
+export function generateASTC(input: string, blockSize?: BlockSize, quality?: Quality, colorProfile?: ColorProfile, options?: string[]): Promise<string>;
 
-export function generateEtc(input: string, format?: Format, effort?: number, errormetric?: ErrorMetric, options?: string[]): string;
+export function generateETC(input: string, format?: Format, effort?: number, errormetric?: ErrorMetric, options?: string[]): Promise<string>;
