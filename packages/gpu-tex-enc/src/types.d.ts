d04c4838eb10375601ddc85f1ccfaaa220055f05
ddenysiuk <ddenysiuk@playtika.com>
2024-02-16 13:05:50 +0100

    types fix

--- packages/gpu-tex-enc/src/types.d.ts
+++ packages/gpu-tex-enc/src/types.d.ts
@@ -20,26 +20,47 @@ export type BcOptions = {
 };
 
 export type OutputOptions = {
-    'ASTC'?: AstcOptions,
-    'BC1'?: BcOptions,
-    'BC3'?: BcOptions,
-    'BC4'?: BcOptions,
-    'BC5'?: BcOptions,
-    'BC7'?: BcOptions,
-    'ETC1'?: EtcOptions,
-    'RGB8'?: EtcOptions,
-    'SRGB8'?: EtcOptions,
-    'RGBA8'?: EtcOptions,
-    'SRGBA8'?: EtcOptions,
-    'RGB8A1'?: EtcOptions,
-    'SRGB8A1'?: EtcOptions,
-    'R11'?: EtcOptions
-    'SIGNED_R11'?: EtcOptions
-    'RG11'?: EtcOptions
-    'SIGNED_RG11'?: EtcOptions
+    ASTC?: AstcOptions,
+    BC1?: BcOptions,
+    BC3?: BcOptions,
+    BC4?: BcOptions,
+    BC5?: BcOptions,
+    BC7?: BcOptions,
+    ETC1?: EtcOptions,
+    RGB8?: EtcOptions,
+    SRGB8?: EtcOptions,
+    RGBA8?: EtcOptions,
+    SRGBA8?: EtcOptions,
+    RGB8A1?: EtcOptions,
+    SRGB8A1?: EtcOptions,
+    R11?: EtcOptions
+    SIGNED_R11?: EtcOptions
+    RG11?: EtcOptions
+    SIGNED_RG11?: EtcOptions
 };
 
-export function generate(input: string, outputOptions: OutputOptions): Promise<string>;
+export type GenerationOutput = {
+    ASTC?: string,
+    BC1?: string,
+    BC3?: string,
+    BC4?: string,
+    BC5?: string,
+    BC7?: string,
+    ETC1?: string,
+    RGB8?: string,
+    SRGB8?: string,
+    RGBA8?: string,
+    SRGBA8?: string,
+    RGB8A1?: string,
+    SRGB8A1?: string,
+    R11?: string
+    SIGNED_R11?: string
+    RG11?: string
+    SIGNED_RG11?: string
+};
+
+
+export function generate(input: string, outputOptions: OutputOptions): Promise<GenerationOutput>;
 
 export function generateBC(input: string, type?: Type, adjust?: boolean, options?: string[]): Promise<string>;
 
